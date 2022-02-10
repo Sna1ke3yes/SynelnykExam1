8b6c7b122656139f9ea045eb7f28e52535ed3de4
SoSmisloM228 <99005398+SoSmisloM228@users.noreply.github.com>
2022-02-10 17:17:23 +0200

    Add files via upload

---
+++ Класы2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Класы2
+{
+    
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress a = new Adress();
+            a.Index = 02125;
+            a.Country = "Ukraine";
+            a.City = "Kyiv";
+            a.Street = "Green";
+            a.House = 9;
+            a.Apartment = 34;
+
+                Console.Write($" Index: {a.Index} " +
+                    $" Country: {a.Country} " +
+                    $" City: {a.City} " +
+                    $" Street: {a.Street} " +
+                    $" House: {a.House}" +
+                    $" Apartment: {a.Apartment}");
+
+
+
+        }
+    }
+}
