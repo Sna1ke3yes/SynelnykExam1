f01187f5baecf57e17bfcda2504c1b2ed78f57b6
SoSmisloM228 <99005398+SoSmisloM228@users.noreply.github.com>
2022-02-12 19:43:20 +0200

    Add files via upload

---
+++ конвертер/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace конвертер
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            double USD = 28.13;
+            double EUR = 31.93;
+            double RUB = 2.67;
+            
+            Converter Curs = new Converter(USD, EUR, RUB);
+        }
+    }
+}
