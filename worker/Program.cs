1e9cbb64f43514196cbb3f1a773b1670a1938012
SoSmisloM228 <99005398+SoSmisloM228@users.noreply.github.com>
2022-02-13 14:19:59 +0200

    Add files via upload

---
+++ worker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee worker = new Employee($"Никита", "Гагауз", 610);
+            Console.WriteLine($"Name is {worker.Name}, surname is {worker.Surname}. DaysWorked: {worker.DaysWorked}");
+            double wages = worker.WagesCalc(Employee.Post.junior);
+            Console.WriteLine($"Wages is {wages}, tax is {wages * 0.18}, post is {Employee.Post.junior}.");
+            Console.ReadKey();
+        }
+    }
+}
