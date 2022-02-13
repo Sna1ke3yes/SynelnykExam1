7e2e879d62efc665cccca1d6086a9f0bdad6e9a4
SoSmisloM228 <99005398+SoSmisloM228@users.noreply.github.com>
2022-02-13 15:45:26 +0200

    Add files via upload

---
+++ USER/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User one = new User("Killer", "Daniil", "Synelnyk", 18, DateTime.Now);
+            Console.WriteLine($"Login: {one.Login}, Name: {one.Name}, Surname: {one.Surname},Age: {one.Age}, Reristration: {one.registr} ");
+            Console.ReadKey();
+        }
+       
+    }
+}
