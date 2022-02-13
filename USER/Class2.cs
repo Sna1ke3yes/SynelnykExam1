037130134529783b05087845530295edcf2bac4a
SoSmisloM228 <99005398+SoSmisloM228@users.noreply.github.com>
2022-02-13 15:44:29 +0200

    Add files via upload

---
+++ USER/Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USER
+{
+     class User
+    {
+        public string Login;
+        public string Name;
+        public string Surname;
+        public int Age;
+        private readonly DateTime Registr;
+        public DateTime registr
+        {
+            get { return Registr; }
+            set { registr = value; }
+        }
+
+
+
+
+        public User(string login, string name, string surname, int age, DateTime registr)
+        {
+            Login = login;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Registr = registr;
+        }
+    }
+
+}
